2fc9a9c212ffc4e7ec1bd6ab3418d296f70d4a14
DevYsh <ysh940129@gmail.com>
2024-10-02 10:44:54 +0900

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card(props) {
+  return (
+    <>
+      <li className="concept">
+        <img src={props.image} alt={props.title} />
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </li>
+    </>
+  );
+}
